263eb9ecfc9c867e7db25226cce503330371bc89
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-12-27 11:14:42 +0100

    Try to use the 'musicbrainz' package

--- tests/testthat/test-get_cds.R
+++ tests/testthat/test-get_cds.R
@@ -5,3 +5,21 @@ test_that("use", {
   expect_true("formation" %in% names(get_cds()))
   expect_true("year" %in% names(get_cds()))
 })
+
+test_that("All CD titles must be in MusicBrainz", {
+
+  # artists <- musicbrainz::search_artists("K3")
+  # k3_mbid <- artists[which(artists$disambiguation == "Flemish all girl pop group"), ]$mbid
+  # releases <- musicbrainz::browse_releases_by("artist", k3_mbid)
+  # # recordings <- musicbrainz::browse_recordings_by("artist", k3_mbid) # songs
+  # musicbrainz_titles <- releases$title
+  #
+  # # ??? releases <- musicbrainz::lookup_release_by_id(k3_mbid, includes = "recordings")
+  #
+  #
+  # titles <- get_cds()$cd_title
+  #
+  # found_titles <- titles[titles %in% musicbrainz_titles]
+  # missing_titles <- titles[! titles %in% musicbrainz_titles]
+
+})
