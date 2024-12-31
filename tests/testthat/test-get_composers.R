489cc925c3b37e68730fb0970212c23d3a24f061
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-12-31 11:03:36 +0100

    All song titles match between 'get_songs' and 'get_composers'

--- tests/testthat/test-get_composers.R
+++ tests/testthat/test-get_composers.R
@@ -1,6 +1,14 @@
-test_that("multiplication works", {
+test_that("minimal use", {
   expect_silent(get_composers())
   expect_true(tibble::is_tibble(get_composers()))
   expect_true("song_title" %in% names(get_composers()))
   expect_true("composer" %in% names(get_composers()))
+})
+
+test_that("all song titles match get_songs()", {
+  composer_song_titles <- unique(get_composers()$song_title)
+  song_titles <- unique(get_songs()$song_title)
+  expect_true(
+    all(composer_song_titles %in% song_titles)
+  )
 })
